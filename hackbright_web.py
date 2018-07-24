301fb8ef9590f5ca845a04cf3ca643d1e1c2594f
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-24 15:42:02 -0700

    Add html template and Jinja

---
+++ hackbright_web.py
@@ -0,0 +1,23 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    # return "{} is the GitHub account for {} {}".format(github, first, last)
+    return render_template("student_info.html", first=first, last=last, github=github)
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
