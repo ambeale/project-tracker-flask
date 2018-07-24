a32439b881dc82e232fa2000c3ab70e2b8dd6ae2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-24 16:26:04 -0700

    Add grade listings on student page

--- hackbright_web.py
+++ hackbright_web.py
@@ -14,9 +14,13 @@ def get_student():
     github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
+    grades = hackbright.get_grades_by_github(github)
 
     # return "{} is the GitHub account for {} {}".format(github, first, last)
-    return render_template("student_info.html", first=first, last=last, github=github)
+    return render_template("student_info.html", first=first,
+    											last=last,
+    											github=github,
+    											grades=grades)
 
 @app.route("/student-search")
 def get_student_form():
